ca986244e41bae877ad1b6d12e0d7fdae5a74d8f
SagaciousLittle <39272516+SagaciousLittle@users.noreply.github.com>
2019-03-21 14:31:43 +0800

    新增阿里司法拍卖

--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
 import axios from './http'
 import Sequelize from 'sequelize'
 import moment from 'moment'
+import jsdom from 'jsdom'
+import iconv from 'iconv-lite'
 import Info from './Info'
+
+const {
+  JSDOM,
+} = jsdom
 
 const {
   DATEBASE,
@@ -19,7 +25,7 @@ async function getSeq () {
     timezone: '+08:00',
     logging: false,
     pool: {
-      max: 5,
+      max: 50,
       min: 0,
       acquire: 30000,
       idle: 10000
@@ -36,11 +42,36 @@ async function getSeq () {
     }
   })
 
+  // 阿里司法拍卖
+  const AliJudicialSell = sequelize.define('AliJudicialSell', {
+    all: {
+      type: Sequelize.INTEGER
+    },
+    beijing: {
+      type: Sequelize.INTEGER
+    },
+    tianjin: {
+      type: Sequelize.INTEGER
+    },
+    shanghai: {
+      type: Sequelize.INTEGER
+    },
+    hebei: {
+      type: Sequelize.INTEGER
+    },
+    shanxi: {
+      type: Sequelize.INTEGER
+    }
+  })
+
   await DollarExchangeRate.sync()
+  await AliJudicialSell.sync()
 
   try {
     await sequelize.authenticate()
-    let r
+
+    // dollar
+    let t1
     setInterval(async function () {
       const res = await axios.get(`https://hq.sinajs.cn/rn=${Date.now()}list=fx_susdcny`)
       // 数据，0为时间，2为汇率
@@ -52,15 +83,50 @@ async function getSeq () {
           time
         }
       })
-      if (r !== res.data && t.length === 0) {
-        DollarExchangeRate.create({
-          time,
-          rate: d[2]
-        })
-        r = res.data
-        console.log(`time: ${moment(time).format('YYYY-MM-DD HH:mm:ss')}, rate: ${d[2]}`)
+      if (t1 !== res.data && t.length === 0) {
+        try {
+          DollarExchangeRate.create({
+            time,
+            rate: d[2]
+          })
+          t1 = res.data
+          console.log(`美元汇率信息 - time: ${moment(time).format('YYYY-MM-DD HH:mm:ss')}, rate: ${d[2]}`)
+        } catch (e) {
+          console.error(e.message)
+        }
       }
     }, 10000)
+
+    // house
+    let t2
+    setInterval(async () => {
+      const resHouseInfo = (await Promise.all([
+        axios.get('https://sf.taobao.com/item_list.htm?category=50025969'),
+        axios.get('https://sf.taobao.com/item_list.htm?category=50025969&city=&province=%B1%B1%BE%A9'),
+        axios.get('https://sf.taobao.com/item_list.htm?category=50025969&city=&province=%CC%EC%BD%F2'),
+        axios.get('https://sf.taobao.com/item_list.htm?category=50025969&city=&province=%C9%CF%BA%A3'),
+        axios.get('https://sf.taobao.com/item_list.htm?category=50025969&city=&province=%BA%D3%B1%B1'),
+        axios.get('https://sf.taobao.com/item_list.htm?category=50025969&city=&province=%C9%BD%CE%F7'),
+      ])).map(o => {
+        return new JSDOM(o.data).window.document.querySelector('h1').textContent.match(/\d+/g)[0]
+      })
+      if (t2 !== resHouseInfo.join()) {
+        try {
+          AliJudicialSell.create({
+            all: resHouseInfo[0],
+            beijing: resHouseInfo[1],
+            tianjin: resHouseInfo[2],
+            shanghai: resHouseInfo[3],
+            hebei: resHouseInfo[4],
+            shanxi: resHouseInfo[5]
+          })
+          t2 = resHouseInfo.join()
+          console.log(t2)
+        } catch (e) {
+          console.error(e.message)
+        }
+      }
+    }, 1000 * 60)
   } catch (e) {
     console.error(e.message)
     setTimeout(getSeq, 1000 * 60 * 5)
